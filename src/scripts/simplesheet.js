da687ef99bc1a1c36b622fe3f526efcda6892194
ewfian <ewfian@vip.qq.com>
2018-01-26 00:07:01 +0800

    temp: test scroll animation for mobile phone

--- src/scripts/simplesheet.js
+++ src/scripts/simplesheet.js
@@ -1,5 +1,8 @@
 import Element from './element';
 import Div from './div';
+import Vector2 from './vector2';
+import easingFuncs from './easingfuncs';
+import easingfuncs from './easingfuncs';
 
 let SimpleSheet = function (query) {
     this.container = document.querySelector(query);
@@ -50,13 +53,21 @@ let SimpleSheet = function (query) {
         }
     });
 
+    let startTime, endTime;
     let startX, startY;
+    let offsetX, offsetY;
+
+    let positionX, positionY;
+
     let scrollLeft = 0,
         scrollTop = 0;
     scrollElement.addEventListener('touchstart', (e) => {
         e.preventDefault();
         startX = e.changedTouches[0].pageX;
         startY = e.changedTouches[0].pageY;
+        scrollLeft = scrollElement.scrollLeft;
+        scrollTop = scrollElement.scrollTop;
+        startTime = e.timeStamp;
     }, false);
     scrollElement.addEventListener('touchmove', (e) => {
         //https://www.tjvantoll.com/2012/08/19/onscroll-event-issues-on-mobile-browsers/
@@ -65,41 +76,79 @@ let SimpleSheet = function (query) {
         e.preventDefault();
         let rect = scrollElement.getBoundingClientRect();
         let moveEndX = e.changedTouches[0].pageX,
-            moveEndY = e.changedTouches[0].pageY,
-            tapX = moveEndX - startX,
-            tapY = moveEndY - startY;
+            moveEndY = e.changedTouches[0].pageY;
+
+        offsetX = moveEndX - startX;
+        offsetY = moveEndY - startY;
+
+        positionX = offsetX;
+        positionY = offsetY;
 
         if (moveEndX > rect.left &&
             moveEndY > rect.top &&
             moveEndX < (rect.left + rect.width) &&
             moveEndY < (rect.top + rect.height)) {
 
+            //if (Math.abs(tapX) < 2 && Math.abs(tapY) < 2) return;
 
-            if (Math.abs(tapX) < 2 && Math.abs(tapY) < 2) return;
-
-            let angle = Math.atan2(tapY, tapX) * 180 / Math.PI;
+            let angle = Math.atan2(positionY, positionX) * 180 / Math.PI;
             if (angle >= -135 && angle <= -45) {
                 // to up
-                tapX = 0;
+                positionX = 0;
             } else if (angle > 45 && angle < 135) {
                 // to down
-                tapX = 0;
+                positionX = 0;
             } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
                 // to left
-                tapY = 0;
+                positionY = 0;
             } else if (angle >= -45 && angle <= 45) {
                 // to right
-                tapY = 0;
+                positionY = 0;
             }
-            // Math.max(Math.min(inputX, MAX), MIN)
-            scrollElement.scrollTop = Math.max(Math.min(
-                scrollTop -= tapY / (scrollElement.scrollHeight / 800),
-                scrollElement.scrollHeight), 0);
 
-            scrollElement.scrollLeft = Math.max(Math.min(
-                scrollLeft -= tapX / (scrollElement.scrollWidth / 800),
-                scrollElement.scrollWidth), 0);
+            let top = scrollTop - positionY;
+            let left = scrollLeft - positionX; /// (scrollElement.scrollWidth / scrollElement.scrollHeight);
+
+            // Math.max(Math.min(inputX, MAX), MIN)
+            scrollElement.scrollTop = Math.max(Math.min(top, scrollElement.scrollHeight), 0);
+            scrollElement.scrollLeft = Math.max(Math.min(left, scrollElement.scrollWidth), 0);
         }
+    }, false);
+
+    scrollElement.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        endTime = e.timeStamp;
+
+        let duration = endTime - startTime;
+
+        let magtitude = new Vector2(offsetX, offsetY).length();
+
+        let speedRadius = magtitude / duration * 5;
+
+        let loopCount = 100 * speedRadius;
+
+        let currentScroll = new Vector2(scrollElement.scrollLeft, scrollElement.scrollTop);
+
+        let index = 0;
+        let loopAction = () => {
+            let position = new Vector2(positionX, positionY);
+            let length = position.length();
+            if (length > 0) {
+                let norm = position.divide(position.length());
+                let x = 1 - index / loopCount;
+                let easeRadius = easingFuncs.circularOut(x);
+                currentScroll = currentScroll.subtract(norm.multiply(speedRadius * easeRadius));
+                //window.requestAnimationFrame(() => {
+                scrollElement.scrollLeft = currentScroll.x;
+                scrollElement.scrollTop = currentScroll.y;
+                //});
+            }
+            if (index < loopCount) {
+                index++;
+                setTimeout(loopAction, 30);
+            }
+        };
+        setTimeout(loopAction, 30);
     }, false);
 };
 

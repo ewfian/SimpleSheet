7770022fdaf632288c72af13f870c808df56729a
EchoFan <Echo.fan@grapecity.com>
2018-02-02 18:09:35 +0800

    temp: just stash code

--- src/scripts/mvvm/observer.js
+++ src/scripts/mvvm/observer.js
@@ -52,18 +52,25 @@ export function Observer(obj) {
     obj._proxy = new Proxy(obj, handler);
 }
 
+let proxyDep = null;
 var handler = {
     get(target, key) {
         if (typeof target[key] === 'object' && target[key] !== null) {
-            return new Proxy(target[key], handler);
+            return (function () {
+                proxyDep = new Depend();
+                return new Proxy(target[key], handler);
+            })();
         } else {
+            var dependTarget = Depend.target;
+            if (dependTarget) {
+                proxyDep.addSub(dependTarget);
+                console.log(target,key);
+            }
             return target[key];
         }
     },
     set(target, key, value) {
-        console.log(target);
-        console.log(key);
-        console.log(value);
+        console.log(target, key, value);
         return true;
     }
 };
